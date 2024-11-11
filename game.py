df8b4481e1ae43663f509dff6f61a0ad0681a246
wxn151 <wxn.npc@gmail.com>
2024-11-11 12:44:22 -0600

    replace game.py
    include play again and improve the logic iteration

--- game.py
+++ game.py
@@ -3,177 +3,160 @@ import random
 from collections import deque
 import os
 
-board = [
-    ['-', '-', '-', '0'],
-    ['-', '-', '-', '1'],
-    ['-', '-', '-', '2'],
-    ['A', 'B', 'C']
-]
-
 #to reinit the game
-class Regret:
-    _rndr = deque(['a0', 'b0', 'c0', 'a1', 'b1', 'c1', 'a2', 'b2', 'c2'])
-    _plyrFigure = ('Ø', 'X')
+class TicTacToe:
+    def __init__(self):
+        self.board = [
+            ['-', '-', '-', '0'],
+            ['-', '-', '-', '1'],
+            ['-', '-', '-', '2'],
+            ('A', 'B', 'C')
+        ]
+        self.rndr = deque(['a0', 'b0', 'c0', 'a1', 'b1', 'c1', 'a2', 'b2', 'c2'])
+        self.rndrIndex = {'a0': '00', 'b0': '01', 'c0': '02', 'a1': '10', 'b1': '11', 'c1': '12', 'a2': '20', 'b2': '21', 'c2': '22'}
+        self.plyrBox = {'cpu': 'non', 'human': 'non'}
+        self.plyrAvaliable = ['Ø', 'X']
+        self.cpuTurn = False
 
-rndr = Regret._rndr
-rndrIndex = {'a0': '00', 'b0': '01', 'c0': '02', 'a1': '10', 'b1': '11', 'c1': '12', 'a2': '20', 'b2': '21', 'c2': '22'}
-plyrBox = {'cpu': 'non', 'human': 'non'}
-plyrAvaliable = Regret._plyrFigure
-cpuTurn = False
+    def gameover(self, figure):
+        #ways to win
+        score = False
+        bias = 0
+        prompter = 0
+        if (len(self.rndr) < 6):
+            while True:
 
-def gameover(figure):
-    #ways to win
-    score = False
-    bias = 0
-    if (len(rndr) < 6):
+                # horizontal
+                while (prompter >= 2):
+                    line = self.board[prompter][0:3]
+                    for n in line:
+                        if (n == figure):
+                            bias+=1
+                    if (bias > 2):
+                        break;
+                    else:
+                        bias = 0
+                        prompter += 1
+
+
+                # vertical
+                prompter = 2
+                while (prompter >= 0):
+                    for n in self.board:
+                        if (n[prompter] == figure):
+                            bias += 1
+                    if (bias > 2):
+                        score = figure
+                        break;
+                    else:
+                        bias = 0
+                        prompter -=1
+
+
+                #diagonal
+                prompter = 0
+                for n in self.board:
+                    if (n[prompter] == figure):
+                        bias += 1
+                    if (prompter<2):
+                        prompter += 1
+                if (bias > 2):
+                    break;
+                else:
+                    bias = 0
+                for n in self.board:
+                    if (n[prompter] == figure):
+                        bias += 1
+                    if (prompter > 0):
+                        prompter -= 1
+                if (bias > 2):
+                    break;
+                else:
+                    bias = 0
+                break;
+
+        if (bias > 2):
+            score = True
+        return score
+
+    def load(self):
+        print("The board løcks like")
+        for row in self.board:
+            for element in row:
+                print(element, end=' ')
+            print()
+
+    ####### RULES ######
+    # one turn per player
+    # x fgr always start
+    def press_start(self):
+
+        choice = input('Choose your figure Ø or X (for Ø introduce o, for X introduce x, any other character it return an random figure)\n')
+        choice = choice.lower()
+        if (choice == 'x'):
+            self.plyrBox['human'] = 'X'
+            self.plyrBox['cpu'] = 'Ø'
+        elif (choice == 'o'):
+            self.plyrBox['cpu'] = 'X'
+            self.plyrBox['human'] = 'Ø'
+        else:
+            self.plyrBox['cpu'] = random.choice(self.plyrAvaliable)
+            self.plyrAvaliable.remove(self.plyrBox['cpu'])
+            self.plyrBox['human'] = self.plyrAvaliable[0]
+
+        if (self.plyrBox['cpu'] == 'X'):
+            self.cpuTurn = True
+
         while True:
+            avaliable = ', '.join(self.rndr)
+            if (len(self.rndr) == 0):
+                print("No body wins ▲....▲")
+                break
+            ############################## TIC TAC TOE ###############################
+            if (self.cpuTurn):
+                move = random.choice(self.rndr)
+                self.rndr.remove(move)
+                i = self.rndrIndex[move]
+                k = list(i)
+                self.board[int(k[0])][int(k[1])] = self.plyrBox['cpu']
+                self.cpuTurn = False
+                # end game
+                if (self.gameover(self.plyrBox['cpu'])):
+                    self.load()
+                    print('Winner the CPU (you sucks!!!)')
 
-            # horizontal
-            line = board[0][0:3]
-            for n in line:
-                if (n == figure):
-                    bias+=1
-            if (bias > 2):
-                break;
             else:
-                bias = 0
-            line = board[1][0:3]
-            for n in line:
-                if (n == figure):
-                    bias += 1
-            if (bias > 2):
-                break;
-            else:
-                bias = 0
-            line = board[2][0:3]
-            for n in line:
-                if (n == figure):
-                    bias += 1
-            if (bias > 2):
-                break;
-            else:
-                bias = 0
+                self.load()
+                move = input("Your turn. Avalaiable boxes:\n" + avaliable + "\n")
 
-            # vertical
-            for n in board:
-                if (n[0] == figure):
-                    bias += 1
-            if (bias > 2):
-                score = figure
-                break;
-            else:
-                bias = 0
-            for n in board:
-                if (n[2] == figure):
-                    bias += 1
-            if (bias > 2):
-                break;
-            else:
-                bias = 0
-            for n in board:
-                if (n[1] == figure):
-                    bias += 1
-            if (bias > 2):
-                break;
-            else:
-                bias = 0
-
-            #diagonal
-            pos = 0
-            for n in board:
-                if (n[pos] == figure):
-                    bias += 1
-                if (pos<2):
-                    pos += 1
-            if (bias > 2):
-                break;
-            else:
-                bias = 0
-            for n in board:
-                if (n[pos] == figure):
-                    bias += 1
-                if (pos > 0):
-                    pos -= 1
-            if (bias > 2):
-                break;
-            else:
-                bias = 0
-            break;
-
-    if (bias > 2):
-        score = True
-    return score
-
-def load():
-    print("The board løcks like")
-    for row in board:
-        for element in row:
-            print(element, end=' ')
-        print()
-
-
-
-####### RULES ######
-# one turn per player
-
-choice = input('Choose your figure Ø or X (for Ø introduce o, for X introduce x, any other character it return an random figure)\n')
-choice = choice.lower()
-if (choice == 'x'):
-    plyrBox['human'] = 'X'
-    plyrBox['cpu'] = 'Ø'
-elif (choice == 'o'):
-    plyrBox['cpu'] = 'X'
-    plyrBox['human'] = 'Ø'
-else:
-    plyrBox['cpu'] = random.choice(plyrAvaliable)
-    plyrAvaliable.remove(plyrBox['cpu'])
-    plyrBox['human'] = plyrAvaliable[0]
-
-if (plyrBox['cpu'] == 'X'):
-    cpuTurn = True
+                if (move in self.rndr):
+                    self.rndr.remove(move)
+                    i = self.rndrIndex[move]
+                    k = list(i)
+                    self.board[int(k[0])][int(k[1])] = self.plyrBox['human']
+                    self.cpuTurn = True
+                    #end game
+                    if (self.gameover(self.plyrBox['human'])):
+                        self.load()
+                        f = input('Congrats, you are the winner\n Enter your name _')[0]
+                        # os.system('cls') # to wndws
+                        os.system('clear') # to lnx
+                        s = input(f'Congrats, you are the winner\n Enter your name {f} _')[0]
+                        os.system('clear')
+                        t = input(f'Congrats, you are the winner\n Enter your Name {f} {s} _')[0]
+                        os.system('clear')
+                        arcade = f + s + t
+                        print(f'bye {arcade}. thx to play')
+                        break
+                else:
+                    print("Introduce an avaliable option\n")
 
 while True:
-    avaliable = ', '.join(rndr)
-    if (len(rndr) == 0):
-        print("No body wins ..▲..")
+    onArcade = TicTacToe()
+    onArcade.press_start()
+    plyAgain = input('Press enter to play again.')
+    if (plyAgain != ''):
         break
-    ############################## TIC TAC TOE ###############################
-    if (cpuTurn):
-        move = random.choice(rndr)
-        rndr.remove(move)
-        i = rndrIndex[move]
-        k = list(i)
-        board[int(k[0])][int(k[1])] = plyrBox['cpu']
-        cpuTurn = False
-        # end game
-        if (gameover(plyrBox['cpu'])):
-            load()
-            print('Winner the CPU (you sucks!!!)')
-            break
-
     else:
-        load()
-        move = input("Your turn. Avalaiable boxes:\n" + avaliable + "\n")
-
-        if (move in rndr):
-            rndr.remove(move)
-            i = rndrIndex[move]
-            k = list(i)
-            board[int(k[0])][int(k[1])] = plyrBox['human']
-            cpuTurn = True
-            #end game
-            if (gameover(plyrBox['human'])):
-                load()
-                f = input('Congrats, you are the winner\n Enter your name _')[0]
-                # os.system('cls') # to wndws
-                os.system('clear') # to lnx
-                s = input(f'Congrats, you are the winner\n Enter your name {f} _')[0]
-                os.system('clear')
-                t = input(f'Congrats, you are the winner\n Enter your Name {f} {s} _')[0]
-                os.system('clear')
-                arcade = f + s + t
-                print(f'bye {arcade}. thx to play')
-                break
-        else:
-            print("Introduce an avaliable option\n")
+        del(onArcade)
 
